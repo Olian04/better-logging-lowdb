c7813f586f10c99910978ee65cf4b7b6bf10f4a9
oliver <olian04@home-server.lan>
2018-10-27 13:11:42 +0000

    1.0.0

---
+++ index.js
@@ -0,0 +1,23 @@
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const init = (dbPath) => {
+    const adapter = new FileSync(dbPath);
+    const db = low(adapter);
+    db.defaults({ logs: [] })
+      .write();
+    
+    return ({
+        db,
+        middleware = ({
+            onLogEmitted: log => {
+                if (db) {
+                    db.get('logs')
+                        .push({ time: Date.now(), log })
+                        .write();
+                }
+            }
+        })        
+    });
+}
+module.exports = init;
+module.exports.default = init;
